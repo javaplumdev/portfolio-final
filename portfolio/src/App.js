3b6b95822812367bee8e0f587adc6a084eafd3bc
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-05-27 14:18:29 +0800

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,76 +12,123 @@ import {
 	ZoomIn,
 } from 'react-scroll-motion';
 
+import MotionCircles from './components/MotionCircles';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function App() {
 	const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
-	const FadeUp = batch(Fade(), Move(), Sticky());
+
+	const [mousePosition, setMousePosition] = useState({
+		x: 0,
+		y: 0,
+	});
+
+	const variants = {
+		default: {
+			y: mousePosition.y - 16,
+			x: mousePosition.x - 16,
+		},
+		text: {
+			height: 150,
+			width: 150,
+			x: mousePosition.x - 75,
+			y: mousePosition.y - 75,
+			backgroundColor: 'purple',
+		},
+	};
+
+	const [cursorVariant, setCursorVariant] = useState('default');
+
+	useEffect(() => {
+		function mouseMove(e) {
+			setMousePosition({
+				x: e.clientX,
+				y: e.clientY,
+			});
+		}
+
+		window.addEventListener('mousemove', mouseMove);
+
+		return () => {
+			window.removeEventListener('mousemove', mouseMove);
+		};
+	}, []);
+
+	const textEnter = () => setCursorVariant('text');
+	const textLeave = () => setCursorVariant('default');
 
 	return (
 		<ScrollContainer>
+			<motion.div
+				variants={variants}
+				animate={cursorVariant}
+				className="cursor"
+			></motion.div>
 			<div style={{ backgroundColor: 'white' }}>
 				<ScrollPage page={0}>
 					<Animator animation={batch(Fade(), Sticky(), MoveOut(0, -400))}>
 						<div className="text-container">
-							<h1 style={{ fontSize: '8em' }}>Heyo.</h1>
-							<p style={{ fontSize: '22px' }}>Providing nice websites.</p>
+							<h1 onMouseEnter={textEnter} onMouseLeave={textLeave}>
+								Heyo.
+							</h1>
+							<p
+								onMouseEnter={textEnter}
+								onMouseLeave={textLeave}
+								style={{ fontSize: '22px' }}
+							>
+								Providing nice websites.
+							</p>
 						</div>
 						<div className="circle-container">
+							<MotionCircles />
+						</div>
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								marginTop: '4em',
+							}}
+						>
+							<motion.div className="mouse"></motion.div>
 							<motion.div
-								className="circle"
-								animate={{ x: [0, 200, 150, 0], y: [0, 200, 200, 0] }}
-								transition={{ repeat: Infinity, duration: 5 }}
-							></motion.div>
-							<motion.div
-								className="circle"
-								animate={{ x: [0, 26, 20, 0], y: [0, -23, 33, 0] }}
-								transition={{ repeat: Infinity, duration: 2 }}
-							></motion.div>
-							<motion.div
-								className="circle"
-								animate={{ x: [0, -400, -30, 0], y: [0, 11, 66, 0] }}
-								transition={{ repeat: Infinity, duration: 3 }}
-							></motion.div>
-							<motion.div
-								className="circle"
-								animate={{ x: [26, 99, 48, 60], y: [70, 200, -88, 82] }}
-								transition={{ repeat: Infinity, duration: 4 }}
+								initial={{ y: 10 }}
+								animate={{ y: 25, opacity: 0.1 }}
+								transition={{ duration: 1, repeat: Infinity }}
+								className="wheel"
 							></motion.div>
 						</div>
+						<motion.p
+							initial={{ opacity: 1 }}
+							animate={{ opacity: 0.1 }}
+							transition={{ duration: 1, repeat: Infinity }}
+							style={{
+								textAlign: 'center',
+								fontSize: '1.2em',
+								marginTop: '1em',
+							}}
+						>
+							Scroll down
+						</motion.p>
 					</Animator>
 				</ScrollPage>
 			</div>
 			<div style={{ color: 'white' }}>
 				<ScrollPage page={1}>
 					<Animator animation={ZoomInScrollOut}>
-						<h1
+						{/* <h1
+							onMouseEnter={textEnter}
+							onMouseLeave={textLeave}
 							style={{
 								fontSize: '6em',
 							}}
 						>
 							I'm Charlito
-						</h1>
-						<p>A aspiring frontend developer</p>
+						</h1> */}
+						{/* <p>A aspiring frontend developer</p> */}
 					</Animator>
 				</ScrollPage>
 			</div>
-
-			<ScrollPage page={3}>
-				<Animator animation={FadeUp}>
-					<span style={{ fontSize: '3.5em', color: '#ffff99' }}>Skills ?</span>
-				</Animator>
-			</ScrollPage>
-			<ScrollPage page={4}>
-				<Animator animation={FadeIn}>
-					<span style={{ fontSize: '2em', color: 'white' }}>Node Js</span>
-				</Animator>
-			</ScrollPage>
-			<ScrollPage page={5}>
-				<Animator animation={batch(Fade(), Sticky(), MoveOut(0, -50))}>
-					<span style={{ fontSize: '1.5em', color: '#db4dff' }}>Express</span>
-				</Animator>
-			</ScrollPage>
 		</ScrollContainer>
 	);
 }
